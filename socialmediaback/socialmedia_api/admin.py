6b49178d6dd12979c36fdbde9f37dbfbcae7d9f2
Kevin Trang <ktrngcode@gmail.com>
2022-02-04 03:30:51 -0500

    models registered. superuser created

--- socialmediaback/socialmedia_api/admin.py
+++ socialmediaback/socialmedia_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+admin.site.register(User)
+
+from .models import Post
+admin.site.register(Post)
+
+from .models import Comment
+admin.site.register(Comment)
