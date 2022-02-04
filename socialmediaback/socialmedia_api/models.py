2505ff3958cdc67b02d6c4dd718d534447ce0e5f
Kevin Trang <ktrngcode@gmail.com>
2022-02-04 03:19:27 -0500

    fixed errors with the models. successfully migrated

--- socialmediaback/socialmedia_api/models.py
+++ socialmediaback/socialmedia_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.BigAutoField(primary_key=True, on_delete=models.CASCADE)
+    user_id = models.BigAutoField(primary_key=True)
     email = models.EmailField(max_length=256, unique=True)
     password = models.CharField(max_length=64)
     first_name = models.CharField(max_length=32)
@@ -14,18 +14,20 @@ class User(models.Model):
     ##will work on this later (https://www.geeksforgeeks.org/python-uploading-images-in-django/)##
 
     # not sure if this will work tbh, will try and find out
-    friends = models.ForeignKey('self', on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self')
 
-    posts = models.OneToManyField(Post, on_delete=models.CASCADE)
+    posts = models.ForeignKey('Post', on_delete=models.CASCADE)
 
 class Post(models.Model):
-    id = models.BigAutoField()
-    author = models.ForeignKey(User)
+    post_id = models.BigAutoField(primary_key=True)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
     body = models.CharField(max_length=640)
-    comments = models.OneToManyField(Comment, on_delete=models.CASCADE)
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE)
     likes = models.IntegerField()
 
 class Comment(models.Model):
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    comment_id = models.BigAutoField(primary_key=True)
+    author = models.OneToOneField('User', on_delete=models.CASCADE)
+    postedto = models.ForeignKey('Post', on_delete=models.CASCADE)
     body = models.CharField(max_length=640)
     likes = models.IntegerField()
--- socialmediaback/socialmediaback/settings.py
+++ socialmediaback/socialmediaback/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'socialmedia_api'
+    'socialmedia_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
