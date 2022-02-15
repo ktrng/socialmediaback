3742753765060205e3c6441fa642aa51c0eaa102
Kevin Trang <ktrngcode@gmail.com>
2022-02-15 15:20:09 -0500

    views.py started

--- socialmediaback/socialmedia_api/views.py
+++ socialmediaback/socialmedia_api/views.py
@@ -1,3 +1,21 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from .serializers import UserSerializer
+from .models import User
+
+from .serializers import PostSerializer
+from .models import Post
+
+from .serializers import CommentSerializer
+from .models import Comment
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    querset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
